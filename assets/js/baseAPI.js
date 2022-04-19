37e9fd79f88abaf8de666b99b47569a0e3919830
DZW0203 <zhenwu_dong@163.com>
2022-04-19 17:08:56 +0800

    这是完成了登录和注册功能的开发

---
+++ assets/js/baseAPI.js
@@ -0,0 +1,7 @@
+// 每次调用$.get(),$.post()或$.ajax()时
+// 会先调用ajaxPrefilter（）这个函数
+// 在这个函数中，可以拿到我们给ajax提供的配置对象
+$.ajaxPrefilter(function(options){
+    // 在发送真正的ajax请求之前，统一拼接请求的根路径
+    options.url = 'http://127.0.0.1:3007' + options.url
+})
